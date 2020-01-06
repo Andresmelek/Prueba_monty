63453400d97f623d1469751a284751867ae331f8
Doouh <ake.giraldo@hotmail.com>
2020-01-05 23:39:46 -0500

    Fix stack null

--- functions_1.c
+++ functions_1.c
@@ -40,7 +40,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	unsigned int sum;
 
-	if ((*stack == NULL) || (stack == NULL) || ((*stack)->next == NULL))
+	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		free(stack);
