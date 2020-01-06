523918846b9226c0207f189625a1cde609e06174
Doouh <ake.giraldo@hotmail.com>
2020-01-06 00:03:44 -0500

    Fix functions_2

--- functions_2.c
+++ functions_2.c
@@ -16,6 +16,11 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_number);
+                exit(EXIT_FAILURE);
+        }
 	module = (*stack)->next->n % (*stack)->n;
 
 	pop(stack, 0);
