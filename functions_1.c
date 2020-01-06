a3b2a734f54cc13a2748b12566800b06ddc19a02
Andresmelek <candres.isaza@gmail.com>
2020-01-05 22:42:56 -0500

    Fiing pop

--- functions_1.c
+++ functions_1.c
@@ -17,8 +17,11 @@ void pop(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if (tmp->next == NULL && tmp->prev == NULL)
+	if (tmp->next == NULL)
+	{
 		(*stack) = NULL;
+		free(tmp);
+	}
 	else
 	{
 		tmp = tmp->next;
